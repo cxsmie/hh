dc2bd5df998c37ee53d70cadcf9f1b2ea45d2248
cxsmie <94294635+cxsmie@users.noreply.github.com>
2023-02-21 18:11:21 +0100

    Update splix.lua

--- splix.lua
+++ splix.lua
@@ -143,7 +143,7 @@ function library:new(props)
 	local screen = utility.new(
 		"ScreenGui",
 		{
-			Name = tostring(math.random(0,999999))..tostring(math.random(0,999999)),
+			Name = name,
 			DisplayOrder = 9999,
 			ResetOnSpawn = false,
 			ZIndexBehavior = "Global",
